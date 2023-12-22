eb9ffea4c4e4966f17bca9375ab815b6b6ec1c1a
ZygeSam <sakinbarnessamuel@gmail.com>
2023-12-22 11:54:09 +0100

    added cart functionality

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
          <!----page-header----->
          <div class="mini_cart_togglers fixed_cart">
             <div class="mini-cart-count">
-               0
+               {{count(session()->get('cart')['products'])}}
             </div>
             <i class="icon-shopping-cart"></i>
          </div>
@@ -890,7 +890,26 @@
       <div class="cart_content_box">
          <div class="contnet_cart_box">
             <div class="widget_shopping_cart_content">
-               <p class="woocommerce-mini-cart__empty-message">No products in the cart.</p>
+               @forelse(session()->get('cart')['products'] as $product)
+                  <tr class="woocommerce-cart-form__cart-item cart_item p-5">
+                     
+                     <td class="product-remove">
+                     <a href="{{route('cart.remove', $product[0]['id'])}}" class="remove" aria-label="Remove this item" data-product_id="2650" data-product_sku="">×</a>						
+                     </td>
+                     <td class="product-name" data-title="Product">
+                     <p>Package : {{$product[0]['name']}}</p>
+                     <p>Starting Date:@php echo date("Y-m-d", strtotime("now"));  @endphp</p> 
+                     <p> Daily Data: {{$product[0]['data_gb']}}GB</p>
+                     <p>Throttle: 1Mbps</p>					
+                     </td>
+                     <td class="product-price" data-title="Price">
+                     <span class="woocommerce-Price-amount amount"><bdi><span class="woocommerce-Price-currencySymbol"></span>${{$product[0]['price_usd']}}</bdi></span>						
+                     </td>
+                  </tr>
+                  @empty
+                  <p class="woocommerce-mini-cart__empty-message">No products in the cart.</p>
+               @endforelse
+               
             </div>
          </div>
       </div>
