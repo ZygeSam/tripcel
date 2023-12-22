1ad36180359d0866286241d8045d6c43e8d8f51b
ZygeSam <sakinbarnessamuel@gmail.com>
2023-12-22 08:48:14 +0100

    added transaction cloud credentials

--- config/payment.php
+++ config/payment.php
@@ -2,5 +2,9 @@
 return[
     "paystack"=>[
         "paystack_secret_key" => env('PAYSTACK_TEST_SK', null),
+    ],
+    "transaction-cloud"=>[
+        "apiLogin" => env('TRANSACTION_CLOUD_API_LOGIN', null),
+        "apiPassword" => env('TRANSACTION_CLOUD_API_PASSWORD', null),
     ]
 ];
