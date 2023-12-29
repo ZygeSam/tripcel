31d868fabc07466aa43e204c3d85fa3be872f9aa
ZygeSam <sakinbarnessamuel@gmail.com>
2023-12-29 19:23:22 +0100

    cached product result

--- app/Services/EsimProductService.php
+++ app/Services/EsimProductService.php
@@ -2,6 +2,7 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 use App\Traits\CurlableTrait;
 use App\Services\ESimAuthService;
 
@@ -26,7 +27,29 @@ class ESimProductService
     {
         $this->params = http_build_query(['country'=>$country]);
         $this->url = config('esim.maya-api.apiLiveUrl')."/partners/v1/products?$this->params";
-        return $this->curl($this->header, $this->url);
+        // Attempt to retrieve products from cache first
+        $cacheKey = 'esim_products_' . md5($this->url);
+        $cachedProducts = Cache::remember($cacheKey, now()->addHours(1), function () {
+            // Fetch products from API if not found in cache
+            return $this->curl($this->header, $this->url);
+        });
+
+        return $cachedProducts;
+    }
+
+    public function getAllRegionProducts($region=null)
+    {
+        $this->params = http_build_query(['region'=>$region]);
+        $this->url = config('esim.maya-api.apiLiveUrl')."/partners/v1/products?$this->params";
+        // Attempt to retrieve products from cache first
+        $cacheKey = 'esim_products_' . md5($this->url);
+        $cachedProducts = Cache::remember($cacheKey, now()->addHours(1), function () {
+            // Fetch products from API if not found in cache
+            return $this->curl($this->header, $this->url);
+        });
+
+        return $cachedProducts;
+        // return $this->curl($this->header, $this->url);
     }
 
     public function getAProduct(string $id)
