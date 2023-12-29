f3fa606ebdf5a571d3325827d52f8b778dc45705
ZygeSam <sakinbarnessamuel@gmail.com>
2023-12-29 16:16:55 +0100

    Password active link eneabled

--- resources/views/dashboards/client/password.blade.php
+++ resources/views/dashboards/client/password.blade.php
@@ -71,10 +71,10 @@
         <div class="col-md-12">
             <ul class="nav nav-pills flex-column flex-md-row mb-3">
             <li class="nav-item">
-                <a class="nav-link active" href="javascript:void(0);"><i class="bx bx-user me-1"></i> Account</a>
+                <a class="nav-link" href="javascript:void(0);"><i class="bx bx-user me-1"></i> Account</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="./password.html"
+                <a class="nav-link active" href="./password.html"
                 ><i class="bx bx-bell me-1"></i> Change Password</a
                 >
             </li>
