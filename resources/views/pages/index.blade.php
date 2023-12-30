c8a0842d6b7896fd73101dff6ae93ec5406b560e
ZygeSam <sakinbarnessamuel@gmail.com>
2023-12-30 23:15:49 +0100

    Added the pricing page to the index page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -66,5 +66,153 @@
            </div>
         </div>
      </section>
-
+     
+@endsection
+@section('unlimitedplans')
+<section class="price-section">
+   <div class="row">
+      <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+         <div class="price_plan_box style_one  border border-dark">
+            <div class="tag">Recommended</div>
+            <div class="inner_box">
+               <div class="top">
+                  <h2>Europe</h2>
+                  <p>Pricing plan  starting from</p>
+               </div>
+               <div class="mid">
+                  <h4>$149</h4>
+                  <div class="list_item_box style_two style_list">
+                     <ul class="list-inline">
+                        <li class="list_items pd_bottom_10">
+                           <small class="d-flex align-items-center">
+                              <span class="icon_bx">
+                                 <i class=" icon-checked"></i>
+                              </span>
+                              <a class="nav_link" href="#" target="&quot;_blank&quot;" rel="&quot;nofollow&quot;">
+                                 4G/5G </a>
+                           </small></li>
+                        <li class="list_items pd_bottom_10">
+                           <small class="d-flex align-items-center">
+                              <span class="icon_bx">
+                                 <i class=" icon-checked"></i>
+                              </span>
+                              <a class="nav_link" href="#" target="&quot;_blank&quot;" rel="&quot;nofollow&quot;">
+                                 Data Plans</a>
+                           </small></li>
+                        <li class="list_items pd_bottom_10">
+                           <small class="d-flex align-items-center">
+                              <span class="icon_bx">
+                                 <i class=" icon-checked"></i>
+                              </span>
+                              <a class="nav_link" href="#" target="&quot;_blank&quot;" rel="&quot;nofollow&quot;">
+                                 SMS Plans </a>
+                           </small></li>
+                     </ul>
+                  </div>
+               </div>
+               <div class="bottom">
+                  <a href="{{route('regions', ['region' => 'EUR'])}}" target="_blank" rel="nofollow" class="theme-btn two">
+                     See pricing plans
+                  </a>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+         <div class="price_plan_box style_one tag_enables  border border-dark">
+            <div class="tag">Recommended</div>
+            <div class="inner_box">
+               <div class="top">
+                  <h2>America +</h2>
+                  <p>Pricing plan starting from</p>
+               </div>
+               <div class="mid">
+                  <h4>$249</h4>
+                  <div class="list_item_box style_two style_list">
+                     <ul class="list-inline">
+                        <li class="list_items pd_bottom_10">
+                           <small class="d-flex align-items-center">
+                              <span class="icon_bx">
+                                 <i class=" icon-checked"></i>
+                              </span>
+                              <a class="nav_link" href="#" target="&quot;_blank&quot;" rel="&quot;nofollow&quot;">
+                                 4G/5G </a>
+                           </small></li>
+                        <li class="list_items pd_bottom_10">
+                           <small class="d-flex align-items-center">
+                              <span class="icon_bx">
+                                 <i class=" icon-checked"></i>
+                              </span>
+                              <a class="nav_link" href="#" target="&quot;_blank&quot;" rel="&quot;nofollow&quot;">
+                                 Data Plans</a>
+                           </small></li>
+                        <li class="list_items pd_bottom_10">
+                           <small class="d-flex align-items-center">
+                              <span class="icon_bx">
+                                 <i class=" icon-checked"></i>
+                              </span>
+                              <a class="nav_link" href="#" target="&quot;_blank&quot;" rel="&quot;nofollow&quot;">
+                                 SMS Plans </a>
+                           </small></li>
+                     </ul>
+                  </div>
+               </div>
+               <div class="bottom">
+                  <a href="{{route('regions', ['region' => 'USA'])}}" target="_blank" rel="nofollow" class="theme-btn two">
+                     See pricing plans
+                     </a>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+         <div class="price_plan_box style_one  border border-dark">
+            <div class="tag">Recommended</div>
+            <div class="inner_box">
+               <div class="top">
+                  <h2>Asia</h2>
+                  <p>Pricing plan  starting from</p>
+               </div>
+               <div class="mid">
+                  <h4>$349</h4>
+                  <div class="list_item_box style_two style_list">
+                     <ul class="list-inline">
+                        <li class="list_items pd_bottom_10">
+                           <small class="d-flex align-items-center">
+                              <span class="icon_bx">
+                                 <i class=" icon-checked"></i>
+                              </span>
+                              <a class="nav_link" href="#" target="&quot;_blank&quot;" rel="&quot;nofollow&quot;">
+                                 4G/5G </a>
+                           </small></li>
+                        <li class="list_items pd_bottom_10">
+                           <small class="d-flex align-items-center">
+                              <span class="icon_bx">
+                                 <i class=" icon-checked"></i>
+                              </span>
+                              <a class="nav_link" href="#" target="&quot;_blank&quot;" rel="&quot;nofollow&quot;">
+                                 Data Plans</a>
+                           </small></li>
+                        <li class="list_items pd_bottom_10">
+                           <small class="d-flex align-items-center">
+                              <span class="icon_bx">
+                                 <i class=" icon-checked"></i>
+                              </span>
+                              <a class="nav_link" href="#" target="&quot;_blank&quot;" rel="&quot;nofollow&quot;">
+                                 SMS Plans </a>
+                           </small></li>
+                     </ul>
+                  </div>
+               </div>
+               <div class="bottom">
+                  <a href="{{route('regions', ['region' => 'APC'])}}" target="_blank" rel="nofollow" class="theme-btn two">
+                     See pricing plans
+                     </a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <h3 class="text-center my-5"><a href="{{route('countries')}}">See all 194 countries</a></h3 class="text-center my-5">
+</section>
 @endsection
