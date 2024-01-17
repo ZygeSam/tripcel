072ba4cf7231be8605549f563726f35746abd168
ZygeSam <sakinbarnessamuel@gmail.com>
2024-01-17 12:44:51 +0100

    Verify Email Mail implementation with blade file

---
+++ resources/views/emails/verifyEmail.blade.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href={{asset("assets/images/FII.png")}} type="image/x-icon">
+    <title>Your Data Purchase Confirmation</title>
+</head>
+<body>
+
+    <img src="{{ $message->embed((public_path("assets/images/FI.png")) )}}">
+    
+<p dir="auto" style="font-family:&quot;.AppleSystemUIFont&quot;"><span style="font-family:UICTFontTextStyleBody">Thank you for choosing <b style="font-family:UICTFontTextStyleBody">Tripcel</b>! Please follow the instructions below carefully:</span></p>
+    <p>Click on the link below to verify your mail</p>
+    <p>{{$mailData['message']}}</p>
+    <p>Tis link will expire in 10 minutes</p>
+<p style="font-family:&quot;.AppleSystemUIFont&quot;"><span style="font-family:UICTFontTextStyleBody"></span><br></p>
+
+<p dir="auto" style="font-family:&quot;.AppleSystemUIFont&quot;"><b style="font-family:&quot;.AppleSystemUIFont&quot;"><span style="font-family:UICTFontTextStyleBody"><b style="font-family:UICTFontTextStyleBody">Thank you for choosing TRIPCEL</b>.&nbsp;</span><span style="font-family:UICTFontTextStyleBody"></span></b></p><p dir="auto" style="font-family:&quot;.AppleSystemUIFont&quot;"><b style="font-family:&quot;.AppleSystemUIFont&quot;"><span style="font-family:UICTFontTextStyleBody"><br></span></b></p>
+<p dir="auto" style="font-family:&quot;.AppleSystemUIFont&quot;"><span style="font-family:UICTFontTextStyleBody">Warm regards,</span></p><p dir="auto" style="font-family:&quot;.AppleSystemUIFont&quot;"><span style="font-family:UICTFontTextStyleBody"><b style="font-family:UICTFontTextStyleBody">TRIPCEL Support Team</b></span></p><div class="yj6qo"></div><div class="adL">
+
+</body>
+</html>
