99225713a3ad0d430c1f7923bf25e1eed764cba2
ZygeSam <sakinbarnessamuel@gmail.com>
2024-01-22 16:04:09 +0100

    Webhook done in laravel

--- app/Http/Controllers/EsimProductController.php
+++ app/Http/Controllers/EsimProductController.php
@@ -291,12 +291,12 @@ class EsimProductController extends Controller
     }
 
     public function webhook(Request $request){
-         // Retrieve the request's body and parse it as JSON
-            $input = @file_get_contents("php://input");
-            $event = json_decode($input);
-            // Do something with $event
-            http_response_code(200); // PHP 5.4 or greater
-            return $event;
+        // Parse the event (which is a JSON string) as an object
+        $event = json_decode($request->getContent());
+        // Do something with $event
+        // For example: process the Paystack event, update database, etc.
+
+        return response()->json(['status' => 'success', 'event'=> $event], 200);
     }
 
     public function verifyEmail($email, $gateway){
